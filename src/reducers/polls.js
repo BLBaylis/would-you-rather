127836256755021165ca9cbe0486d2744075b00c
BLBaylis <bradleybaylis@hotmail.com>
2020-04-11 18:59:19 +0100

    Split polls reducer into smaller reducers due to nested data

--- src/reducers/polls.js
+++ src/reducers/polls.js
@@ -1,19 +1,48 @@
 import {RECEIVE_INITIAL_POLLS, ADD_USER_TO_POLL} from '../actions/polls'
 
-const polls = (state = {}, action) => {
+const votes = (voteState = [], {type, userId}) => {
+  switch (type) {
+    case ADD_USER_TO_POLL:
+      return [...voteState, userId]
+    default:
+      return voteState
+  }
+}
+
+const option = (optionState = {}, action) => {
   switch (action.type) {
+    case ADD_USER_TO_POLL:
+      return {
+        ...optionState,
+        votes: votes(optionState.votes, action)
+      }
+    default:
+      return optionState
+  }
+}
+
+const poll = (pollState = {}, action) => {
+  const {type, selectedAnswer} = action
+  switch (type) {
+    case ADD_USER_TO_POLL:
+      return {
+        ...pollState,
+        [selectedAnswer]: option(pollState[selectedAnswer], action)
+      }
+    default:
+      return pollState
+  }
+}
+
+const polls = (state = {}, action) => {
+  const { type, pollId } = action
+  switch (type) {
     case RECEIVE_INITIAL_POLLS:
       return {...state, ...action.polls}
     case ADD_USER_TO_POLL:
       return {
         ...state,
-        [action.pollId]: {
-          ...state[action.pollId],
-          [action.selectedAnswer]: {
-            ...state[action.pollId][action.selectedAnswer],
-            votes: [...state[action.pollId][action.selectedAnswer].votes, action.userId]
-          }
-        }
+        [pollId]: poll(state[pollId], action)
       }
     default:
       return state
