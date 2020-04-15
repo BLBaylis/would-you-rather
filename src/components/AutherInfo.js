0c91c6f99b7061e0f50174852e96475c70fec503
BLBaylis <bradleybaylis@hotmail.com>
2020-04-15 20:58:27 +0100

    Display user's name rather than id

--- src/components/AutherInfo.js
+++ src/components/AutherInfo.js
@@ -6,7 +6,7 @@ const AutherInfo = ({ author, timestamp }) => {
   const submissionTime = timestampDateObj.toLocaleTimeString('en-UK')
   return (
     <div style = {{borderRight: '1px solid', padding: '2rem'}}>
-      <span style = {{display: 'block'}}>Asked by {author.id}</span>
+      <span style = {{display: 'block'}}>Asked by {author.name}</span>
       <span style = {{display: 'block', marginBottom: '10px'}}>{submissionDate} {submissionTime[1] === ':' ? `0${submissionTime}` : submissionTime}</span>
       <img width = '128px' height = '128px' src = {author.avatarURL} style = {{borderRadius: '50%'}} alt = {`${author.name}'s avatar`}/>
     </div>
